642bbc3f6773ac373b562765db1ae23b30ec7fa0
lovaraj02 <rajubandaru2255@gmail.com>
2025-07-25 17:37:11 +0530

    images

--- the_influence_of_artificial_intelligence/settings.py
+++ the_influence_of_artificial_intelligence/settings.py
@@ -129,16 +129,42 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# # STATICFILES_DIRS = [
+# #     os.path.join(BASE_DIR, 'Template', 'images')]
+
+
+
+# # STATIC_ROOT = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# 
+
+import os
+
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR,'Template/images')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Template', 'images')]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'Template/media')
 
 # STATIC_ROOT = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_URL = '/static/'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
