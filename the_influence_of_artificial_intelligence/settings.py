d28cecd5e310f4bbe74f2405a361d6a74df1c564
lovaraj02 <rajubandaru2255@gmail.com>
2025-11-26 19:15:45 +0530

    debug true

--- the_influence_of_artificial_intelligence/settings.py
+++ the_influence_of_artificial_intelligence/settings.py
@@ -7,10 +7,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
 
 # for live
-DEBUG = False
+# DEBUG = False
 
 # for local
-# DEBUG = True
+DEBUG = True
 
 # REQUIRED FOR RENDER
 ALLOWED_HOSTS = ["*"]
